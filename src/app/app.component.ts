1a17bd42ecc170cdfd6f37335b9f525456095051
Mark Jayson Noguerra <mark.n@translations.ph>
2020-04-18 23:31:27 +0800

    setup image classfier pwa web app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Prediction } from '../prediction';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as tf from '@tensorflow/tfjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'img-classification';
+
+  imageSrc: string;
+  @ViewChild('img', {static: true}) imageEl: ElementRef;
+  @ViewChild('video', {static: true}) video: ElementRef;
+
+  predictions: Prediction[];
+
+  model: any;
+  loading = true;
+
+
+  constructor() { }
+
+  async ngOnInit() {
+    console.log('loading mobilenet model...');
+    this.model = await mobilenet.load();
+    console.log('Sucessfully loaded model');
+    this.loading = false;
+
+    setInterval(async () => {
+      this.predictions = await this.model.classify(this.video.nativeElement);
+      await tf.nextFrame();
+    }, 3000);
+  }
+
+  async fileChangeEvent(event) {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (res: any) => {
+        this.imageSrc = res.target.result;
+        setTimeout(async () => {
+          const imgEl = this.imageEl.nativeElement;
+          this.predictions = await this.model.classify(imgEl);
+          console.log(this.predictions);
+        }, 0);
+
+      };
+    }
+
+  }
+
+  async ngAfterViewInit() {
+    const vid = this.video.nativeElement;
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then((stream) => {
+          vid.srcObject = stream;
+
+        })
+        .catch((err0r) => {
+          console.log('Something went wrong!');
+        });
+    }
+  }
+
+  
 }
